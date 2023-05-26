40e5a942294c29b761991c7972357a18ebfa4170
mk2376 <mk2376@student.uni-lj.si>
2023-05-26 16:36:11 +0200

    Fixed wrong file moving

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         <h2 className="text-5xl text-white font-mono font-medium my-10 text-center">Dedicated to planning and handling advertising</h2>
         <div className="flex justify-around">
           <Link href="/advertisements/overview" className="bg-primary-500 hover:bg-primary-600 text-white font-bold py-10 px-16 rounded text-2xl flex mr-6">
-            <img src="/assets/svg/bookmark_white.svg" className="px-2"></img>Enter as advertising client
+            <img src="/assets/img/bookmark_white.svg" className="px-2"></img>Enter as advertising client
           </Link>
           <Link href="/websites/overview" className="bg-white hover:bg-slate-50 text-black font-bold py-10 px-16 rounded text-2xl flex">
-            <img src="/assets/svg/bookmark_black.svg" className="px-2"></img>Enter as advertising client
+            <img src="/assets/img/bookmark_black.svg" className="px-2"></img>Enter as advertising client
           </Link>
         </div>
       </div>
