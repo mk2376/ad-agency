fdd0ac75de953ee45979522099417fd4c947112c
mk2376 <mk2376@student.uni-lj.si>
2023-05-28 15:33:19 +0200

    Ad details

--- dapp/app/ads/view/[id]/page.tsx
+++ dapp/app/ads/view/[id]/page.tsx
@@ -12,21 +12,21 @@ export default function Ad({ params }: { params: { id: string } }) {
             Advertisement #{params.id}
         </p>
         
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-6xl font-bold text-gray-950">
             Track the status of your advertisement
         </h1>
 
-        <p className="mt-3 text-2xl">
+        <p className="mt-9 mb-24 text-2xl text-gray-800">
             Check how your advertisement is progressing
         </p>
 
-        <div className="grid grid-cols-4 gap-4">
-            <Card status={true} title='Advertisement submitted' description='Advertisement has been submited successfully and is stored on the blockchain along with InterPanetary Fie System.'></Card>
-            <Card status={true} title='AI model picked' description='AI model that will perform the advertisement classification has been picked.'></Card>
-            <Card status={true} title='AI model performs the classification' description='Classification by the previously picked AI model has been performed.'></Card>
-            <Card status={false} title='Advertisement is accepted' description='Congratulations Your advertisement has been approved.'></Card>
-            <Card status={false} title='List of relevant websites' description='Websites that have the relevant target audience have been found.'></Card>
-            <Card status={false} title='Advertisement is displayed on the website' description='Your advertisement is visile on the website. Creck it out <a href="">here<a/>.'></Card>
+        <div className="grid grid-cols-3 gap-4">
+            <Card status={true} title='1. Advertisement submitted' description='Advertisement has been submited successfully and is stored on the blockchain along with InterPanetary Fie System.'></Card>
+            <Card status={true} title='2 .AI model picked' description='AI model that will perform the advertisement classification has been picked.'></Card>
+            <Card status={true} title='3. AI model performs the classification' description='Classification by the previously picked AI model has been performed.'></Card>
+            <Card status={false} title='4. Advertisement is accepted' description='Congratulations Your advertisement has been approved.'></Card>
+            <Card status={false} title='5. List of relevant websites' description='Websites that have the relevant target audience have been found.'></Card>
+            <Card status={false} title='6. Advertisement is displayed on the website' description='Your advertisement is visile on the website. Creck it out <a href="">here<a/>.'></Card>
         </div>
       </main>
     </div>
--- dapp/components/Card.tsx
+++ dapp/components/Card.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import CheckBadgeIcon from '@heroicons/react/24/solid/CheckBadgeIcon';
+import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
 
 type CardType = {
     status: boolean
@@ -8,17 +9,26 @@ type CardType = {
 
 export default function Card({status, title, description}: CardType) {
     return (
-        <div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-            <Image 
-                src={ status ? "assets/img/check_badge.svg": "assets/img/x_circle.svg" } 
-                alt={ status ? "Check badge": "X circle"}
-                height={30}
-                width={30}
-            ></Image> 
-            <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+        <div className="block rounded-lg text-left bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+            <div className='rounded-lg bg-slate-100 p-2 w-min mb-4'>
+                { status ?
+                    <CheckBadgeIcon
+                        height={30}
+                        width={30}
+                        color='black'
+                    />
+                    :
+                    <XCircleIcon
+                        height={30}
+                        width={30}
+                        color='black'
+                    />
+                }
+            </div>
+            <h5 className="mb-2 text-xl leading-tight text-neutral-800 font-bold">
                 { title }
             </h5>
-            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            <p className="mb-4 text-base text-neutral-800">
                 { description }
             </p>
         </div>
